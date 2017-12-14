4c85dcca4ad693507bd14a33d6ef10cbfbb40669
Ivan Volkov <ivan375447132510@yandex.by>
2017-12-14 03:09:45 +0300

    commit 2

---
+++ js/js.js
@@ -0,0 +1,61 @@
+
+
+$(document).ready(function(){
+    $(".strelki").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+$(document).ready(function(){
+    $("li").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+$(".imgDiv").on("click", ".bagrColor", function(){
+    $(".imgDiv").removeClass("activeDiv")
+    $(this).parent().addClass("activeDiv")
+})
+
+// таб 
+
+
+
+  $('div.tabs__caption').on('click', 'img:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+ console.log('работает');
+  });
+
+
+
+// таб конец
+
+
+
+
+
+
+
+
+
